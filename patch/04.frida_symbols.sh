0d1925c13b76b173bbed64d98e79881749b7abb4
zhengyuqin <zhengyuqin@youmi.net>
2020-08-03 01:03:49 +0800

    更新规则

--- patch/04.frida_symbols.sh
+++ patch/04.frida_symbols.sh
@@ -1,1 +1,3 @@
 sed -i 's/\"_frida_\"/\"_agdata_\"/g' releng/devkit.py
+sed -i 's/\".config.so\"/\".fig.so\"/g' frida-core/lib/gadget/gadget.vala
+sed -i 's/\".config\"/\".fig\"/g' frida-core/lib/gadget/gadget.vala
